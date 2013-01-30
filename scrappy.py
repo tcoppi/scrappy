8b579b3300c25c1f953057e6a4a8b3dc319360d4
Landon Fowles <landon@lfowles.org>
2013-01-30 14:08:53 -0600

    Using the logging library now

--- scrappy.py
+++ scrappy.py
@@ -7,6 +7,7 @@ import ConfigParser
 DEFAULT_CONFIG = "default.conf"
 CONFIG_NAME = ".scrappyrc"
 
+import logging
 import os, os.path
 import ssl
 import sys
@@ -17,148 +18,151 @@ import thread, threading
 #import irclib
 from irclib import client as ircclient
 
-################################################################################
-#set to False to turn off debugging to stdout
-DEBUG = True
 
-def debug(msg):
-    if DEBUG:
-        print msg
+# Logging guidelines and levels
+# DEBUG - Python debugging (e.g., printing list of connected servers on startup)
+# INFO - Anything we would want to see during normal operation (e.g., startup/reboot messages)
+# WARNING - Things we would rather not happen (e.g., using a non-SSL connection)
+# ERROR - Deviations from normal operation (e.g., failed to connect to a server)
+# CRITICAL - Scrappy gonna die! But not before spitting out a final goodbye! (e.g., SIGINT)
+logging.basicConfig(level=logging.DEBUG)
 
-################################################################################
 
+irc_logger = logging.getLogger("irclib.client")
+# Change me if you want to see IRC DEBUG lines
+irc_logger.setLevel(logging.INFO)
 
 #this is our main bot class.  Once scrappy.py is called, an instance of this
 #class (our bot) gets created and some initialization is done.  The real work is
 #done via modules that get loaded here.
 class scrappy:
         def __init__(self):
-                # TODO: Use logging module
-                debug("Scrappy bot started.")
 
-                if not os.path.exists(CONFIG_NAME):
-                    print "Error: Configuration file '%s' not found." % CONFIG_NAME
-                    print "Copy %s to %s and modify as necessary." % (DEFAULT_CONFIG, CONFIG_NAME)
-                    sys.exit(1)
-                self.config = ConfigParser.SafeConfigParser()
-                self.config.read(CONFIG_NAME)
+            self.logger = logging.getLogger("scrappy")
 
-                self.servers = {}
-                required_items = ["cmdchar","nickname","username","realname",
-                                    "server","port","channels", "ssl"]
-                for server in self.config.sections():
-                    self.servers[server] = {}
-                    for (k,v) in self.config.items(server):
-                        self.servers[server][k] = v
+            # TODO: Use logging module
+            self.logger.info("Scrappy bot started.")
 
-                    # Ok ok, so ssl is already a key, but I want to coerce it to bool
-                    self.servers[server]["ssl"] = self.config.getboolean(server, "ssl")
+            if not os.path.exists(CONFIG_NAME):
+                self.logger.critical("Error: Configuration file '%s' not found." % CONFIG_NAME)
+                self.logger.critical("Copy %s to %s and modify as necessary." % (DEFAULT_CONFIG, CONFIG_NAME))
+                sys.exit(1)
+            self.config = ConfigParser.SafeConfigParser()
+            self.config.read(CONFIG_NAME)
 
-                    # Sanity check
-                    errors = False
-                    for item in required_items:
-                        if item not in self.servers[server]:
-                            errors = True
-                            print "Error: %s not found in configuration." % item
-                        if errors:
-                            sys.exit(1)
+            self.servers = {}
+            required_items = ["cmdchar","nickname","username","realname",
+                                "server","port","channels", "ssl"]
+            for server in self.config.sections():
+                self.servers[server] = {}
+                for (k,v) in self.config.items(server):
+                    self.servers[server][k] = v
 
-                    self.servers[server]["channels"] = self.servers[server]["channels"].split()
-                    self.servers[server]["port"] = int(self.servers[server]["port"])
+                # Ok ok, so ssl is already a key, but I want to coerce it to bool
+                self.servers[server]["ssl"] = self.config.getboolean(server, "ssl")
 
-                self.ircsock = '' #this will be the socket
-                self.lock = threading.Lock()
+                # Sanity check
+                errors = False
+                for item in required_items:
+                    if item not in self.servers[server]:
+                        errors = True
+                        self.logger.critical("Error: %s not found in configuration." % item)
+                    if errors:
+                        sys.exit(1)
 
-                #our event lists.
-                #each module adds functions to be called to these events.
-                #each event handler calls all of the functions within its list.
-                self.events = {"connect": {},
-                                "disconnect": {},
-                                "error": {},
-                                "invite": {},
-                                "join": {},
-                                "kick": {},
-                                "load": {},
-                                "mode": {},
-                                "msg": {},
-                                "part": {},
-                                "ping": {},
-                                "pong": {},
-                                "privmsg": {},
-                                "privnotice": {},
-                                "pubmsg": {},
-                                "privmsg": {},
-                                "pubnotice": {},
-                                "quit": {},
-                                "tick": {}}
+                self.servers[server]["channels"] = self.servers[server]["channels"].split()
+                self.servers[server]["port"] = int(self.servers[server]["port"])
 
-                self.modules = {}
-                #TODO: Necessary with using __import__?
-                sys.path.append(os.path.join(os.getcwd(), "modules"))
+            self.ircsock = '' #this will be the socket
+            self.lock = threading.Lock()
 
-                #need to load a few modules here - the rest are done elsewhere
-                self.load_module("modmanage")
-                self.load_module("core")
+            #our event lists.
+            #each module adds functions to be called to these events.
+            #each event handler calls all of the functions within its list.
+            self.events = {"connect": {},
+                            "disconnect": {},
+                            "error": {},
+                            "invite": {},
+                            "join": {},
+                            "kick": {},
+                            "load": {},
+                            "mode": {},
+                            "msg": {},
+                            "part": {},
+                            "ping": {},
+                            "pong": {},
+                            "privmsg": {},
+                            "privnotice": {},
+                            "pubmsg": {},
+                            "privmsg": {},
+                            "pubnotice": {},
+                            "quit": {},
+                            "tick": {}}
 
-                #start the bot
-                self.__main()
+            self.modules = {}
+            #TODO: Necessary with using __import__?
+            sys.path.append(os.path.join(os.getcwd(), "modules"))
+
+            #need to load a few modules here - the rest are done elsewhere
+            self.load_module("modmanage")
+            self.load_module("core")
+
+            #start the bot
+            self.__main()
 
         ########################################################################
         def __main(self):
-                """The real work.  Initialize our connection and register events."""
-                # Save arguments
-                self.argv = sys.argv
-                # Create a new socket
-                self.ircsock = ircclient.IRC()
+            """The real work.  Initialize our connection and register events."""
+            # Save arguments for rebooting (see 'core' module)
+            self.argv = sys.argv
+            # Create a new socket
+            self.ircsock = ircclient.IRC()
 
+            for server in self.servers:
+                server = self.servers[server]
+                try:
+                    if server["ssl"]:
+                        factory = ircclient.connection.Factory(wrapper=ssl.wrap_socket)
+                    else:
+                        self.logger.warning("Hey, we really don't like you not using SSL")
+                        factory = ircclient.connection.Factory()
+
+                    connection = self.ircsock.server().connect(server["server"], server["port"], server["nickname"],
+                                                                username=server["username"], ircname=server["realname"],
+                                                                connect_factory=factory)
+                except ircclient.ServerConnectionError, err:
+                    self.logger.exception("Failed to connect to %s:%s" % (server["server"], server["port"]))
+                    connection = None
+
+                #if all goes well, register handlers
+                #TODO: What ones are we missing?
+                if connection is not None:
+                    connection.add_global_handler("welcome", self.on_connect)
+                    connection.add_global_handler("disconnect", self.on_disconnect)
+                    connection.add_global_handler("error", self.on_error)
+                    connection.add_global_handler("invite", self.on_invite)
+                    connection.add_global_handler("join", self.on_join)
+                    connection.add_global_handler("kick", self.on_kick)
+                    connection.add_global_handler("mode", self.on_mode)
+                    connection.add_global_handler("part", self.on_part)
+                    connection.add_global_handler("ping", self.on_ping)
+                    connection.add_global_handler("pong", self.on_pong)
+                    connection.add_global_handler("privmsg", self.on_privmsg)
+                    connection.add_global_handler("privnotice", self.on_privnotice)
+                    connection.add_global_handler("pubmsg", self.on_privmsg)
+                    connection.add_global_handler("quit", self.on_quit)
+                    connection.execute_every(5, self.on_tick, arguments=(connection,))
+
+                server["connection"] = connection
+
+            #enter main event loop after this
+            #no code after here
+            try:
+                self.ircsock.process_forever()
+            except KeyboardInterrupt:
                 for server in self.servers:
                     server = self.servers[server]
-                    try:
-                        # SSL! hardcoded
-                        if server["ssl"]:
-                            factory = ircclient.connection.Factory(wrapper=ssl.wrap_socket)
-                        else:
-                            print "Hey, we really don't like you not using SSL" # TODO: log warning
-                            factory = ircclient.connection.Factory()
-
-                        connection = self.ircsock.server().connect(server["server"], server["port"], server["nickname"],
-                                                                    username=server["username"], ircname=server["realname"],
-                                                                    connect_factory=factory)
-                    except ircclient.ServerConnectionError, err:
-                        print err
-                        print "Nonfatal Error: %s" % err
-                        print "Failed to connect to %s:%s" % (server["server"], server["port"])
-                        connection = None
-
-                    #if all goes well, register handlers
-                    #TODO: What ones are we missing?
-                    if connection is not None:
-                        connection.add_global_handler("welcome", self.on_connect)
-                        connection.add_global_handler("disconnect", self.on_disconnect)
-                        connection.add_global_handler("error", self.on_error)
-                        connection.add_global_handler("invite", self.on_invite)
-                        connection.add_global_handler("join", self.on_join)
-                        connection.add_global_handler("kick", self.on_kick)
-                        connection.add_global_handler("mode", self.on_mode)
-                        connection.add_global_handler("part", self.on_part)
-                        connection.add_global_handler("ping", self.on_ping)
-                        connection.add_global_handler("pong", self.on_pong)
-                        connection.add_global_handler("privmsg", self.on_privmsg)
-                        connection.add_global_handler("privnotice", self.on_privnotice)
-                        connection.add_global_handler("pubmsg", self.on_privmsg)
-                        connection.add_global_handler("quit", self.on_quit)
-                        connection.execute_every(5, self.on_tick, arguments=(connection,))
-
-                    server["connection"] = connection
-
-                #enter main event loop after this
-                #no code after here
-                try:
-                        self.ircsock.process_forever()
-                except KeyboardInterrupt:
-                    for server in self.servers:
-                        server = self.servers[server]
-                        server["connection"].quit("BAIL OUT!!")
+                    server["connection"].quit("BAIL OUT!!")
 
         ########################################################################
         def get_server(self, conn):
@@ -176,7 +180,7 @@ class scrappy:
             """Call this with an event_type and a function to call when that event_type happens."""
 
             if not event_type in self.events:
-                debug("I don't know what an %s event is." % event_type)
+                self.logger.error("I don't know what an %s event is." % event_type)
                 return
 
             event_dict = self.events[event_type]
@@ -235,50 +239,34 @@ class scrappy:
         ########################################################################
         def on_error(self, conn, event):
             self.process_events("error", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Error received: %s" % event.arguments)
 
         ########################################################################
         def on_invite(self, conn, event):
             self.process_events("invite", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Received an invite: %s" % event.arguments)
 
         ########################################################################
         def on_join(self, conn, event):
             self.process_events("join", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("User joined: %s" % event.arguments)
 
         ########################################################################
         def on_kick(self, conn, event):
             self.process_events("kick", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Someone was kicked: %s" % event.arguments)
 
         ########################################################################
         def on_mode(self, conn, event):
             self.process_events("mode", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Mode change: %s" % event.arguments)
 
         ########################################################################
         def on_part(self, conn, event):
             self.process_events("part", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Part: %s" % event.arguments)
 
         ########################################################################
         def on_ping(self, conn, event):
             self.process_events("ping", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Ping: %s" % event.arguments)
 
         ########################################################################
         def on_pong(self, conn, event):
             self.process_events("pong", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Pong: %s" % event.arguments)
 
         ########################################################################
         def on_privmsg(self, conn, event):
@@ -324,30 +312,25 @@ class scrappy:
         ########################################################################
         def on_privnotice(self, conn, event):
             self.process_events("privnotice", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Privnotice: %s" % event.arguments)
 
         ########################################################################
         #right now this isn't used because it's assumed that privmsg == pubmsg
         #this should probably be changed...
         def on_pubmsg(self, conn, event):
             self.process_events("pubmsg", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Pubmsg: % " % event.arguments)
 
         ########################################################################
         def on_quit(self, conn, event):
             self.process_events("quit", conn, event)
-            #TODO: Move this debug to a logging module of some sort
-            debug("Quit: %s" % event.arguments)
-
 
         ################
         #Module Loading#
         ################
+        #TODO: Return booleans, maybe raise exceptions if necessary
         def load_module(self, name):
+            self.logger.debug("Loading module %s." % name)
             if name in self.modules:
-                debug("Actually, module already loaded, reloading.")
+                self.logger.debug("Actually, module %s already loaded, reloading.")
                 return self.reload_module(name)
 
             try:
@@ -355,20 +338,16 @@ class scrappy:
                 module = getattr(package, name)
                 cls = getattr(module, name)
             except AttributeError:
-                print sys.modules
-                print "Error: module %s.%s not found, make sure %s/%s.py exists" % (name,name,name,name)
-                print traceback.print_exc()
+                self.logger.exception("Error: module %s.%s not found, make sure %s/%s.py exists" % (name,name,name,name))
                 return "Sorry, there was an error loading %s." % name
             except ImportError:
-                print "No such module %s" % name
-                print traceback.print_exc()
+                self.logger.exception("No such module %s" % name)
                 return "Sorry, there was an error loading %s." % name
 
             try:
                 self.modules[name] = cls(self)
             except Exception as err:
-                print "Error: Module failed to initialize"
-                print traceback.print_exc()
+                self.logger.exception("Error: Module failed to initialize")
                 return "Sorry, there was an error loading %s." % name
 
             return "Loaded %s." % name
