fc5bede7bb2e95c8fb5145d2fe840d87ee56e647
Landon Fowles <landon@lfowles.org>
2013-01-31 13:08:39 -0600

    Updated core to the latest Module class interface as well as improving the help command to not spam (by requiring a module)

--- modules/core/core.py
+++ modules/core/core.py
@@ -3,56 +3,38 @@
 import os
 import sys
 
-class core(object):
+from module import Module
+
+class core(Module):
     def __init__(self, scrap):
-        self.command_callbacks = {}
+        super(core, self).__init__(scrap)
+
         scrap.register_event("core", "msg", self.distribute)
-        #register commands
-        #scrap.register_event("core", "msg", help_cmd)
-        #scrap.register_event("core", "msg", join_cmd)
+
         self.register_cmd("help", self.help_cmd)
         self.register_cmd("join", self.join_cmd)
         self.register_cmd("reboot", self.reboot_cmd)
 
-    def register_cmd(self, cmd, callback):
-        if cmd not in self.command_callbacks:
-            self.command_callbacks[cmd] = []
-
-        self.command_callbacks[cmd].append(callback)
-
-    def get_help(self, server):
-        docstrings = set()
-        for command in self.command_callbacks:
-            callback_list = self.command_callbacks[command]
-            for callback in callback_list:
-                doc = callback.__doc__
-                doc = "%s%s\n%s" % (server["cmdchar"], command, doc)
-                docstrings.add(doc)
-        return docstrings
-
-    def distribute(self, server, event, bot):
-        if event.iscmd: # event is command
-            command = event.cmd.split(" ")[0]
-            if command in self.command_callbacks:
-                for callback in self.command_callbacks[command]:
-                    callback(server, event, bot)
-
-    def help_cmd(self, server,event,bot):
+    def help_cmd(self, server, event, bot):
         """help - Lists all available commands and their docstrings"""
         c = server["connection"]
 
-        #put the events into a set so we can compute the union
-        #Or maybe we should use sets for the events from the start?
+        if len(event.cmd.split(" ")[1:]) > 0:
+            param = event.cmd.split(" ")[1]
+            if param in bot.modules:
+                module = bot.modules[param]
+                help_set = module.get_help(server)
+                c.privmsg(event.nick, "Module: %s" % module.__class__.__name__)
+                for docstring in help_set:
+                    for part in docstring.split("\n"):
+                        c.privmsg(event.nick,part)
+            else:
+                c.privmsg(event.target, "Module '%s' not loaded, so no help." % param)
+        else:
+            c.privmsg(event.target, "Help list too long, specify a module (see %smodlist.)" % server["cmdchar"])
 
-        for module in bot.modules.values():
-            help_set = module.get_help(server)
-            c.privmsg(event.nick, "Module: %s" % module.__class__.__name__)
-            for docstring in help_set:
-                for part in docstring.split("\n"):
-                    c.privmsg(event.nick,part)
 
-
-    def join_cmd(self, server,event,bot):
+    def join_cmd(self, server, event, bot):
         """join a channel"""
         c = server["connection"]
 
