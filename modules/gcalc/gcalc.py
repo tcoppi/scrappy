ee2bdc9e9f46221fe9f8e9a697dfcce37934bda0
Landon Fowles <landon@lfowles.org>
2013-01-31 14:37:00 -0600

    Updated gcalc to the latest Module class interface.

--- modules/gcalc/gcalc.py
+++ modules/gcalc/gcalc.py
@@ -3,36 +3,37 @@
 
 import urllib, httplib
 
-def init(bot):
-	bot.register_event("gcalc", "msg", gcalc)
+from module import Module
 
-def gcalc(server, args, bot):
-    c = server["connection"]
-    cmd = args[4].split(" ")[0]
-	if cmd == "gcalc":
-	        try:
-	                query = args[4][6:]
-	                query = urllib.urlencode({'q':query})
-	        except:
-	                c.privmsg(args[5], "Error: no query specified.")
-	                return
-	        start = '<h2 class=r style="font-size:138%"><b>'
-	        end = '</b>'
-	        google = httplib.HTTPConnection("www.google.com")
-	        google.request("GET","/search?"+query+"&num=1")
-	        search = google.getresponse()
-	        data = search.read()
-	        #print data
+class gcalc(Module):
+    def __init__(self, bot):
+        super(gcalc, self).__init__(bot)
 
-	        if data.find(start) == -1:
-	                c.privmsg(args[5], "Google Calculator results not found.")
-	                return
-	        else:
-	                begin = data.index(start)
-	                result = data[begin+len(start):begin+data[begin:].index(end)]
-	                result = result.replace("<font size=-2> </font>",",").replace(" &#215; 10<sup>","E").replace("</sup>","").replace("\xa0",",")
-	                c.privmsg(args[5], result)
+        bot.register_event("gcalc", "msg", self.distribute)
 
+        self.register_cmd("gcalc", self.gcalc)
 
+    def gcalc(self, server, event, bot):
+        c = server["connection"]
+        query = event.cmd[6:]
+        query = urllib.urlencode({'q':query})
 
+        if len(query) == 0:
+            c.privmsg(event.source, "Error: no query specified.")
+            return
 
+        start = '<h2 class=r style="font-size:138%"><b>'
+        end = '</b>'
+        google = httplib.HTTPConnection("www.google.com")
+        google.request("GET","/search?"+query+"&num=1")
+        search = google.getresponse()
+        data = search.read()
+        print data
+
+        if data.find(start) == -1:
+            c.privmsg(event.target, "Google Calculator results not found.")
+        else:
+            begin = data.index(start)
+            result = data[begin+len(start):begin+data[begin:].index(end)]
+            result = result.replace("<font size=-2> </font>",",").replace(" &#215; 10<sup>","E").replace("</sup>","").replace("\xa0",",")
+            c.privmsg(event.source, result)
