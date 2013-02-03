5c6cd7d9adeffe4baeaef3c1ee778703114ac368
Landon Fowles <landon@lfowles.org>
2013-02-02 20:25:46 -0600

    Module base can now grab you a database to play with.

--- modules/module.py
+++ modules/module.py
@@ -1,6 +1,32 @@
+import logging
+import os, os.path
+import sqlite3
+
+class ModuleException(Exception):
+    pass
+
+# One object per scrappy!
 class Module(object):
     def __init__(self, scrap):
         self.command_callbacks = {}
+        self.logger = logging.getLogger("scrappy.%s" % self.__class__.__name__)
+
+        if os.path.exists("db") and not os.path.isdir("db"):
+            self.logger.critical("Databases directory 'db' is not a directory!")
+            raise ModuleException("Databases directory 'db' is not a directory.")
+
+        if not os.path.exists("db"):
+            self.logger.debug("Creating database directory.")
+            os.mkdir("db")
+
+        conn = sqlite3.connect('db/%s.db' % self.__class__.__name__)
+        conn.close()
+
+
+    # Don't forget to close it!
+    def get_db(self):
+        conn = sqlite3.connect('db/%s.db' % self.__class__.__name__)
+        return conn
 
     def register_cmd(self, cmd, callback):
         if cmd not in self.command_callbacks:
