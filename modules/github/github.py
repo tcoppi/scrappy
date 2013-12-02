15e6dfb7ff4af756fe1f5982ab862c0a19c56dc5
Landon Fowles <landon@lfowles.org>
2013-12-01 23:50:23 -0600

    Added labels to gh-issue and set the color based on the closest IRC color

--- modules/github/github.py
+++ modules/github/github.py
@@ -1,6 +1,46 @@
 from module import Module
 import urllib2
 import json
+import math
+
+irc_color_map = {
+    "FFFFFF":"1,0", # white
+    "000000":"0,1", # black, white fg
+    "00008B":"0,2", # darkblue, white fg
+    "008000":"1,3", # green
+    "FF0000":"1,4", # red
+    "8B0000":"0,5", # darkred, white fg
+    "800080":"1,6", # purple
+    "FFA500":"1,7", # orange
+    "FFFF00":"1,8", # yellow
+    "32CD32":"1,9", # limegreen
+    "008080":"1,10", # teal
+    "ADD8E6":"1,11", # lightblue
+    "0000FF":"1,12", # blue
+    "FF69B4":"1,13", # hotpink
+    "808080":"1,14", # gray
+    "D3D3D3":"1,15" # lightgrey
+}
+
+def color_distance(c1, c2):
+    c1 = c1[-6:] # strip any leading characters, 0x or #
+    c2 = c2[-6:]
+    r1 = int(c1[0:2],16)
+    g1 = int(c1[2:4],16)
+    b1 = int(c1[4:6],16)
+    r2 = int(c2[0:2],16)
+    g2 = int(c2[2:4],16)
+    b2 = int(c2[4:6],16)
+    return math.sqrt(math.pow(r2-r1,2)+math.pow(g2-g1,2)+math.pow(b2-b1,2))
+
+def closest_color(hex_color):
+    return min(irc_color_map.keys(), key=lambda x: color_distance(x, hex_color))
+
+def closest_irc_color(hex_color):
+    return irc_color_map[closest_color(hex_color)]
+
+def color_text(text, color):
+    return "\x03%s%s\x03" % (color, text)
 
 #TODO: paginate
 class github(Module):
@@ -30,20 +70,26 @@ class github(Module):
         if len(response) == 0:
             c.privmsg(event.target, "No open issues for %s" % repo)
         else:
+            # Get labels
+            label_colors = {} # key name, value color (IRC color, not hex)
             for issue in response:
                 num = issue["number"]
-                name = issue["title"]
+                title = issue["title"]
                 user = issue["user"]["login"]
-                print issue["assignee"]
+                labels = issue["labels"]
+                colored_labels = []
+                for label in labels:
+                    name = label["name"]
+                    if name not in label_colors:
+                        label_colors[name] = closest_irc_color(label["color"])
+
+                    color = label_colors[name]
+                    colored_labels.append(color_text(name, color))
+                label_str = "|".join(colored_labels)
                 if issue["assignee"]:
                     assigned_to = issue["assignee"]["login"]
                 else:
                     assigned_to = None
 
-                c.privmsg(event.target, "Issue %d - opened by %s, assigned to %s - %s" % (num, user, assigned_to, name))
-
-
-        for key in response:
-            print key
-
+                c.privmsg(event.target, "Issue %d %s - opened by %s, assigned to %s - %s" % (num, label_str, user, assigned_to, title))
 
