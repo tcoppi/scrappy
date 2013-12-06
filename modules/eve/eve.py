9a0b574f19f45639b833150a35c61449875c4b10
Landon Fowles <landon@lfowles.org>
2013-12-06 01:28:01 -0600

    Eve module updated to use ServerStates

--- modules/eve/eve.py
+++ modules/eve/eve.py
@@ -28,11 +28,10 @@ class eve(Module):
         self.register_cmd("eve-add", self.eve_add)
 
     def skill(self, server, event, bot):
-        c = server["connection"]
 
-        credentials = self.get_credentials(server["servername"], event.source.nick)
+        credentials = self.get_credentials(server.server_name, event.source.nick)
         if credentials is None:
-            c.privmsg(event.target, "%s isn't in the database, see the %seve-add command" % (event.source.nick, server["cmdchar"]))
+            server.privmsg(event.target, "%s isn't in the database, see the %seve-add command" % (event.source.nick, server.cmdchar))
             return
 
         (eve_id, eve_vcode) = credentials
@@ -44,7 +43,7 @@ class eve(Module):
         skills = api.eve.SkillTree()
 
         for character in result.characters:
-            c.privmsg(event.target, "Training queue for %s" % character.name)
+            server.privmsg(event.target, "Training queue for %s" % character.name)
             queue = auth.char.SkillQueue(characterID=character.characterID)
             for training_skill in queue.skillqueue:
                 # Oh boy, this is going to be horrible.
@@ -52,18 +51,14 @@ class eve(Module):
                     for skill in group.skills:
                         if skill.typeID == training_skill.typeID:
                             time_str = time.strftime("%B %d %H:%M:%S Central",time.localtime(training_skill.endTime))
-                            c.privmsg(event.target, "%s: %s to level %s. Ends %s." % (group.groupName, skill.typeName, training_skill.level, time_str))
-
-
-
+                            server.privmsg(event.target, "%s: %s to level %s. Ends %s." % (group.groupName, skill.typeName, training_skill.level, time_str))
 
     def wallet(self, server, event, bot):
         """ List all character wallets """
-        c = server["connection"]
 
-        credentials = self.get_credentials(server["servername"], event.source.nick)
+        credentials = self.get_credentials(server.server_name, event.source.nick)
         if credentials is None:
-            c.privmsg(event.target, "%s isn't in the database, see the %seve-add command" % (event.source.nick, server["cmdchar"]))
+            server.privmsg(event.target, "%s isn't in the database, see the %seve-add command" % (event.source.nick, server.cmdchar))
             return
 
         (eve_id, eve_vcode) = credentials
@@ -75,25 +70,23 @@ class eve(Module):
         for character in result.characters:
             wallet = auth.char.AccountBalance(characterID=character.characterID)
             isk = wallet.accounts[0].balance
-            c.privmsg(event.target, "Character: %s has %s ISK" % (character.name, isk))
+            server.privmsg(event.target, "Character: %s has %s ISK" % (character.name, isk))
 
     def eve_add(self, server, event, bot):
         """ Add account to eve using <ID> <VCODE> """
-        c = server["connection"]
 
         if len(event.arg) < 2:
-            c.privmsg(event.target, "Not enough arguments, <ID> and <VCODE> required.")
+            server.privmsg(event.target, "Not enough arguments, <ID> and <VCODE> required.")
             return
 
         eve_id = event.tokens[1]
         eve_vcode = event.tokens[2]
 
-        updated = EVEAcct.update(eve_id = eve_id, eve_vcode = eve_vcode).where(EVEAcct.server == server["servername"], EVEAcct.irc_name == event.source.nick)
-        print updated.execute()
+        updated = EVEAcct.update(eve_id = eve_id, eve_vcode = eve_vcode).where(EVEAcct.server == server.server_name, EVEAcct.irc_name == event.source.nick)
         if updated.execute() == 0:
-            EVEAcct(server=server["servername"], irc_name=event.source.nick, eve_id=eve_id, eve_vcode=eve_vcode).save()
+            EVEAcct(server=server.server_name, irc_name=event.source.nick, eve_id=eve_id, eve_vcode=eve_vcode).save()
 
-        c.privmsg(event.target, "Account added!")
+        server.privmsg(event.target, "Account added!")
 
     def get_credentials(self, server, nick):
         try:
