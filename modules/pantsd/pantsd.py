e9ab707442a7b0b333d6c59038f53e96ef8b2c65
Landon Fowles <landon@lfowles.org>
2013-12-06 01:24:16 -0600

    Pantsd updated to use ServerState module

--- modules/pantsd/pantsd.py
+++ modules/pantsd/pantsd.py
@@ -18,22 +18,21 @@ class pantsd(Module):
         self.register_cmd("pants", self.pants)
 
     def pants(self, server, event, bot):
-        c = server["connection"]
 
-        acct = get_account(event.source, server["servername"])
+        acct = get_account(event.source, server.server_name)
         if acct == None:
-            c.privmsg(event.target, "%s, sorry, but you don't have an account." % event.source.nick)
+            server.privmsg(event.target, "%s, sorry, but you don't have an account." % event.source.nick)
             return
         if len(event.arg) > 0:
             status = event.arg[0].lower()
             if status not in ("on","off"):
-                c.privmsg(event.target, "%s, your pants can either be \x02on\x02 or \x02off\x02." % event.source.nick)
+                server.privmsg(event.target, "%s, your pants can either be \x02on\x02 or \x02off\x02." % event.source.nick)
                 return
 
             if status == "on":
-                c.privmsg(event.target, "%s, I see you put your pants on." % event.source.nick)
+                server.privmsg(event.target, "%s, I see you put your pants on." % event.source.nick)
             else:
-                c.privmsg(event.target, "%s, I see you took your pants off." % event.source.nick)
+                server.privmsg(event.target, "%s, I see you took your pants off." % event.source.nick)
 
             q = Pants.update(status=(status=="on")).where(Pants.acct == acct)
             if q.execute() == 0:
@@ -43,8 +42,8 @@ class pantsd(Module):
             try:
                 acct_pants = Pants.get(Pants.acct == acct)
                 if acct_pants.status:
-                    c.privmsg(event.target, "%s, your pants are on!" % event.source.nick)
+                    server.privmsg(event.target, "%s, your pants are on!" % event.source.nick)
                 else:
-                    c.privmsg(event.target, "%s, your pants are off!" % event.source.nick)
+                    server.privmsg(event.target, "%s, your pants are off!" % event.source.nick)
             except Pants.DoesNotExist:
-                c.privmsg(event.target, "%s, I don't know whether your pants are on or off!" % event.source.nick)
+                server.privmsg(event.target, "%s, I don't know whether your pants are on or off!" % event.source.nick)
