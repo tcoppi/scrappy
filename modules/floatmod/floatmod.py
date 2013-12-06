28bd1dacf89751c1568b2cb43672edbe934d566a
Landon Fowles <landon@lfowles.org>
2013-12-05 23:50:33 -0600

    Floatmod module updated to use ServerStates

--- modules/floatmod/floatmod.py
+++ modules/floatmod/floatmod.py
@@ -17,10 +17,9 @@ class floatmod(Module):
         self.register_cmd("bs", self.bs_cmd)
 
     def float_cmd(self, server, event, bot):
-        c = server["connection"]
 
         if len(event.tokens) < 2:
-            c.privmsg(event.target, "Not enough arguments, <NUMBER> required.")
+            server.privmsg(event.target, "Not enough arguments, <NUMBER> required.")
             return
 
         number = event.tokens[1]
@@ -31,7 +30,7 @@ class floatmod(Module):
             try:
                 num = int(number, 16)
             except ValueError:
-                c.privmsg(event.target,("%s: Invalid number") % number)
+                server.privmsg(event.target,("%s: Invalid number") % number)
                 return
 
         tmp = struct.pack("!f", num)
@@ -39,15 +38,13 @@ class floatmod(Module):
         tmp2 = struct.pack("!d", num)
         tmp3 = struct.pack("!q", num)
 
-        c.privmsg(event.target,("%s: 32bit float:0x%s | 2's complement int: 0x%s | 64bit float:0x%s | 2's complement long: 0x%s" % \
+        server.privmsg(event.target,("%s: 32bit float:0x%s | 2's complement int: 0x%s | 64bit float:0x%s | 2's complement long: 0x%s" % \
             (number,binascii.hexlify(tmp),binascii.hexlify(tmp1),binascii.hexlify(tmp2),binascii.hexlify(tmp3))))
 
     def bs_cmd(self, server, event, bot):
 
-        c = server["connection"]
-
         if len(event.tokens) < 2:
-            c.privmsg(event.target, "Not enough arguments, <NUMBER> required.")
+            server.privmsg(event.target, "Not enough arguments, <NUMBER> required.")
             return
 
         number = event.tokens[1]
@@ -58,9 +55,9 @@ class floatmod(Module):
             try:
                 num = int(number, 16)
             except ValueError:
-                c.privmsg(event.target,("%s: Invalid number") % number)
+                server.privmsg(event.target,("%s: Invalid number") % number)
                 return
 
         tmp = struct.pack("<i", num)
 
-        c.privmsg(event.target,("%s: 0x%s" % (number,binascii.hexlify(tmp))))
+        server.privmsg(event.target,("%s: 0x%s" % (number,binascii.hexlify(tmp))))
