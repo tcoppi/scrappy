6df1da63b9ccaac5e48c58f8640019cb27b8921c
Landon Fowles <landon@lfowles.org>
2013-12-06 00:02:15 -0600

    Todo module updated to use ServerStates

--- modules/todo/todo.py
+++ modules/todo/todo.py
@@ -24,83 +24,79 @@ class todo(Module):
 
     # TODO: Paginate if the list gets too long?
     def get_todo(self, server, event, bot):
-        conn = server["connection"]
         if len(event.arg) == 1:
             try:
                 num = int(event.arg[0])
-                todos = Todo.select().where(Todo.server==server["servername"], Todo.channel==event.target, Todo.id==num)
+                todos = Todo.select().where(Todo.server==server.server_name, Todo.channel==event.target, Todo.id==num)
             except ValueError:
                 if event.arg[0] == "all":
-                    todos = Todo.select().where(Todo.server==server["servername"], Todo.channel==event.target).order_by(Todo.id.asc())
+                    todos = Todo.select().where(Todo.server==server.server_name, Todo.channel==event.target).order_by(Todo.id.asc())
                 else:
-                    conn.privmsg(event.target, "%s is either not a valid todo ID or %stodo argument" % (event.arg[0], server["cmdchar"]))
+                    server.privmsg(event.target, "%s is either not a valid todo ID or %stodo argument" % (event.arg[0], server.cmdchar))
                     return
         elif len(event.arg) > 1:
             #TODO: Tell the user what the correct syntax is
-            conn.privmsg(event.target, "Incorrect syntax")
+            server.privmsg(event.target, "Incorrect syntax")
             return
         else:
-            todos = Todo.select().where(Todo.server==server["servername"], Todo.channel==event.target, Todo.done==False).order_by(Todo.id.asc())
+            todos = Todo.select().where(Todo.server==server.server_name, Todo.channel==event.target, Todo.done==False).order_by(Todo.id.asc())
 
         if todos.count() == 0:
-            conn.privmsg(event.target, "No todos for %s@%s" % (event.target, server["servername"]))
+            server.privmsg(event.target, "No todos for %s@%s" % (event.target, server.server_name))
 
         for _todo in todos:
             todo_str = "Todo %d: %s" % (_todo.id, _todo.todo)
             if _todo.done:
                 todo_str = "\x02%s Done\x02" % todo_str
-            conn.privmsg(event.target, todo_str)
+            server.privmsg(event.target, todo_str)
 
     def del_todo(self, server, event, bot):
-        conn = server["connection"]
         if len(event.arg) == 1:
             try:
                 num = int(event.arg[0])
             except ValueError:
-                conn.privmsg(event.target, "%s not a valid todo ID" % num)
+                server.privmsg(event.target, "%s not a valid todo ID" % num)
                 return
         else:
             #TODO: Tell the user correct syntax
-            conn.privmsg(event.target, "Incorrect syntax")
+            server.privmsg(event.target, "Incorrect syntax")
             return
 
-        dq = Todo.delete().where(Todo.server==server["servername"], Todo.channel==event.target, Todo.id==num)
+        dq = Todo.delete().where(Todo.server==server.server_name, Todo.channel==event.target, Todo.id==num)
         deleted = dq.execute()
 
         if deleted:
-            conn.privmsg(event.target, "Todo %d removed" % num)
+            server.privmsg(event.target, "Todo %d removed" % num)
         else:
-            conn.privmsg(event.target, "Todo %d does not exist." % num)
+            server.privmsg(event.target, "Todo %d does not exist." % num)
 
 
     def done_todo(self, server, event, bot):
-        conn = server["connection"]
         if len(event.arg) == 1:
             try:
                 num = int(event.arg[0])
             except ValueError:
-                conn.privmsg(event.target, "%s not a valid todo ID" % num)
+                server.privmsg(event.target, "%s not a valid todo ID" % num)
                 return
         else:
             #TODO: Tell the user correct syntax
-            conn.privmsg(event.target, "Incorrect syntax")
+            server.privmsg(event.target, "Incorrect syntax")
             return
 
-        q = Todo.update(done=True).where(Todo.server==server["servername"], Todo.channel==event.target, Todo.id==num)
+        q = Todo.update(done=True).where(Todo.server==server.server_name, Todo.channel==event.target, Todo.id==num)
         if q.execute() == 0:
-            conn.privmsg(event.target, "Todo %d does not exist." % num)
+            server.privmsg(event.target, "Todo %d does not exist." % num)
         else:
-            conn.privmsg(event.target, "Todo %d set done" % num)
+            server.privmsg(event.target, "Todo %d set done" % num)
 
     def add_todo(self, server, event, bot):
-        conn = server["connection"]
         if len(event.arg) == 0:
             #TODO: Tell the user correct syntax
-            conn.privmsg(event.target, "Incorrect syntax")
+            server.privmsg(event.target, "Incorrect syntax")
             return
 
         todo_text = " ".join(event.arg)
 
-        new_todo = Todo(server=server["servername"], channel=event.target, done=False, todo=todo_text)
+        new_todo = Todo(server=server.server_name, channel=event.target, done=False, todo=todo_text)
         new_todo.save()
-        conn.privmsg(event.target, "Todo %d added" % new_todo.id)
+        server.privmsg(event.target, "Todo %d added" % new_todo.id)
