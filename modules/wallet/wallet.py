cb52b5c08e64d056aab2898d26d1b0b254001b3d
Landon Fowles <landon@lfowles.org>
2013-12-17 19:59:43 -0600

    Refactored wallet module, added command to give .01 to your wallet

--- modules/wallet/wallet.py
+++ modules/wallet/wallet.py
@@ -14,24 +14,33 @@ class wallet(Module):
 
         self.register_cmd("wallet", self.wallet_amt)
         self.register_cmd("transfer", self.transfer)
+        self.register_cmd("gimme", self.gimme)
 
-    def transfer(self, server, event, bot):
-        acct = get_account(event.source, server.server_name)
+    def get_wallet(self, nick, server_name):
+        acct = get_account(nick, server_name)
         try:
             user_wallet = Wallet.get(Wallet.acct == acct)
         except Wallet.DoesNotExist:
             user_wallet = Wallet(acct = acct)
             user_wallet.save()
+
+        return user_wallet
+
+    def transfer(self, server, event, bot):
+        user_wallet = self.get_wallet(event.source, server.server_name)
+
+        server.privmsg(event.target, "Send to %s" % event.args[0])
 
         server.privmsg(event.target, "Error! Not yet implemented because scrappy doesn't know who's who, except for who's talking!")
 
 
     def wallet_amt(self, server, event, bot):
-        acct = get_account(event.source, server.server_name)
-        try:
-            user_wallet = Wallet.get(Wallet.acct == acct)
-        except Wallet.DoesNotExist:
-            user_wallet = Wallet(acct = acct)
-            user_wallet.save()
+        user_wallet = self.get_wallet(event.source, server.server_name)
 
         server.privmsg(event.target, "Your wallet balance is %.6f" % user_wallet.balance)
+
+    def gimme(self, server, event, bot):
+        user_wallet = self.get_wallet(event.source, server.server_name)
+
+        user_wallet.balance += decimal.Decimal(.01)
+        user_wallet.save()
