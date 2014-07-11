17c0f8c920e183992d1da0429e553fb686385abb
Landon Fowles <landon@lfowles.org>
2014-07-11 15:49:07 -0500

    Database initialization routines

--- modules/cah/cah.py
+++ modules/cah/cah.py
@@ -2,37 +2,41 @@
 
 import threading
 from module import Module
+from cards import Cards, init_db
 
 class cah(Module):
+        models = [Cards]
+
 	def __init__(self, scrap):
 		super(cah, self).__init__(scrap)
-		
+
 		self.lock = threading.Lock()
-			
-			
+
+
 		scrap.register_event("cah", "msg", self.distribute)
 		# but how do you handle commands for privmsg?
 		self.register_cmd("cah", self.cah)
-		
-		
+		self.register_cmd("cah-init", self.cah_init)
+
+
 	def cah(self, server, event, bot):
 		'''Main command to parse arguments to !cah trigger.'''
-		
+
 		usage = "start, join, end, add <black|white> <body>, select <1 2 ... N>,"
 
 		if len(event.tokens) >= 2:
 			arg = event.arg[0]
 			msg = event.arg[1:]
-			
+
 			if arg == "start":
 				self.cah_start(server, event, bot)
-				
+
 			elif arg == "join":
 				self.cah_join(server, event, bot)
-				
+
 			elif arg == "end":
 				self.cah_end(server, event, bot)
-				
+
 			elif arg == "add":
 				if len(msg) >= 2:
 					color = msg[0]
@@ -41,7 +45,7 @@ class cah(Module):
 				else:
 					usage = "Add card usage"
 					server.privmsg(event.target, usage)
-					
+
 			elif arg == "select":
 				if len(msg) >= 1:
 					cards = msg[0:]
@@ -49,7 +53,7 @@ class cah(Module):
 				else:
 					usage = "Select card usage"
 					server.privmsg(event.target, usage)
-					
+
 			elif arg == "vote":
 				if len(msg) == 1:
 					#going to need some args here
@@ -57,10 +61,10 @@ class cah(Module):
 				else:
 					usage = "Vote usage"
 					server.privmsg(event.target, usage)
-			
+
 		else:
 			server.privmsg(event.target, usage)
-			
+
 ################################################################################
 # Function prototypes to fill in
 ################################################################################
@@ -68,23 +72,28 @@ class cah(Module):
 	def cah_start(self, server, event, bot):
 		'''Start a new game.'''
 		server.privmsg(event.target, "PLACEHOLDER: starting new game")
-		
+
 	def cah_join(self, server, event, bot):
 		'''Join the current game.'''
 		server.privmsg(event.target, "PLACEHOLDER: joining the game")
-		
+
 	def cah_end(self, server, event, bot):
 		'''Abort the current game.'''
 		server.privmsg(event.target, "PLACEHOLDER: aborting the game")
-		
+
 	def cah_add(self, server, event, bot, color, body):
 		'''Add a card to the database.'''
 		server.privmsg(event.target, "PLACEHOLDER: adding %s card: %s" % (color, body))
-		
+
 	def cah_select(self, server, event, bot, cards):
 		'''Select card(s) to play from your hand.'''
 		server.privmsg(event.target, "PLACEHOLDER: selecting cards %s" % ', '.join(cards))
-		
+
 	def cah_vote(self, server, event, bot, voted):
 		'''Czar voting for group #.'''
 		server.privmsg(event.target, "PLACEHOLDER: czar is voting for %s" % voted)
+
+        def cah_init(self, server, event, bot):
+            server.privmsg(event.target, "Adding cards to DB")
+            init_result = init_db()
+            server.privmsg(event.target, init_result)
--- modules/cah/cards.py
+++ modules/cah/cards.py
@@ -1,9 +1,26 @@
+import peewee
+# needed for init
+import json
+import urllib2
+
 from module import DBModel
 
-import peewee
+JSON_LOCATION = "https://raw.githubusercontent.com/samurailink3/hangouts-against-humanity/master/source/data/cards.js"
 
 class Cards(DBModel):
     color = peewee.TextField()
     body = peewee.TextField()
 
-
+def init_db():
+    try:
+        card_json = urllib2.urlopen(JSON_LOCATION)
+    except URLError:
+        return "Couldn't fetch cards file."
+    card_json = card_json.read().replace("masterCards = ","")
+    try:
+        cards = json.loads(card_json)
+    except ValueError:
+        return "Couldn't parse cards JSON."
+    massaged_dicts = [{'color':'white' if card["cardType"]=='A' else 'black', 'body':card["text"]} for card in cards]
+    Cards.insert_many(massaged_dicts).execute()
+    return "Successfully grabbed cards."
