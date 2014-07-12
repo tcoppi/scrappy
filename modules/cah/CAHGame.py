ae6faabdb4971e81220b0b6f49d491ebe432f370
Matthew Harrison <matthew@matthew-harrison.com>
2014-07-11 22:45:41 -0400

    Tabs to spaces

--- modules/cah/CAHGame.py
+++ modules/cah/CAHGame.py
@@ -1,58 +1,58 @@
 #Cards Against Humanity game engine
 
 class CAHGame:
-	def __init__(self):
-		
-		self.status = "Loaded CAHGame."
-		
-		#flag to keep track of whether or not game is running
-		self.running = False
-		
-		#list of active players in a game
-		self.players = []
-		
-		#dummy with a small deck for testing.
-		#replace with actual card loading from DB later
-		self.deck ={
-			'questions' : [
-				"_? There's an app for that.",
-				"I got 99 problems but _ ain't one.",
-			],
-			'answers' : [
-				"Flying sex snakes.",
-				"Michelle Obama's arms.",
-				"German dungeon porn.",
-				"White people.",
-				"Getting so angry that you pop a boner.",
-				"Freedom of Speech",
-			]
-		}
-		
-		
-	#add a new player to the game
-	def add_player(self, name):
-		self.players.append(Player(name))
-		
-	#start the game
-	def start(self):
-		pass
-		
+        def __init__(self):
+                
+                self.status = "Loaded CAHGame."
+                
+                #flag to keep track of whether or not game is running
+                self.running = False
+                
+                #list of active players in a game
+                self.players = []
+                
+                #dummy with a small deck for testing.
+                #replace with actual card loading from DB later
+                self.deck ={
+                        'questions' : [
+                                "_? There's an app for that.",
+                                "I got 99 problems but _ ain't one.",
+                        ],
+                        'answers' : [
+                                "Flying sex snakes.",
+                                "Michelle Obama's arms.",
+                                "German dungeon porn.",
+                                "White people.",
+                                "Getting so angry that you pop a boner.",
+                                "Freedom of Speech",
+                        ]
+                }
+                
+                
+        #add a new player to the game
+        def add_player(self, name):
+                self.players.append(Player(name))
+                
+        #start the game
+        def start(self):
+                pass
+                
 
-	def draw(self, color):
-		'''Draws a random card of <color> from the databse and returns a Card object.'''
-		pass
+        def draw(self, color):
+                '''Draws a random card of <color> from the databse and returns a Card object.'''
+                pass
 
 
 #Utility class to manage Players
 class Player:
-	def __init__(self, name):
-		self.name = name #Player name (IRC nick)
-		self.score = 0
-		self.hand = {}
-		self.isCzar = False
-		
+        def __init__(self, name):
+                self.name = name #Player name (IRC nick)
+                self.score = 0
+                self.hand = {}
+                self.isCzar = False
+                
 #Utiliy class for a Card
 class Card:
-	def __init__(self, color, body):
-		self.color = color
-		self.body = body
+        def __init__(self, color, body):
+                self.color = color
+                self.body = body
--- modules/cah/cah.py
+++ modules/cah/cah.py
@@ -9,116 +9,116 @@ from CAHGame import CAHGame
 class cah(Module):
         models = [Cards]
 
-	def __init__(self, scrap):
-		super(cah, self).__init__(scrap)
+        def __init__(self, scrap):
+                super(cah, self).__init__(scrap)
 
-		self.lock = threading.Lock()
+                self.lock = threading.Lock()
 
-		self.game = CAHGame()
-			
-		scrap.register_event("cah", "msg", self.distribute)
-		# but how do you handle commands for privmsg?
-		self.register_cmd("cah", self.cah)
-		self.register_cmd("cah-init", self.cah_init)
+                self.game = CAHGame()
+                        
+                scrap.register_event("cah", "msg", self.distribute)
+                # but how do you handle commands for privmsg?
+                self.register_cmd("cah", self.cah)
+                self.register_cmd("cah-init", self.cah_init)
 
 
-	def cah(self, server, event, bot):
-		'''Main command to parse arguments to !cah trigger.'''
+        def cah(self, server, event, bot):
+                '''Main command to parse arguments to !cah trigger.'''
 
-		usage = "new, start, join, end, add <black|white> <body>, select <1 2 ... N>,"
+                usage = "new, start, join, end, add <black|white> <body>, select <1 2 ... N>,"
 
-		if len(event.tokens) >= 2:
-			arg = event.arg[0]
-			msg = event.arg[1:]
+                if len(event.tokens) >= 2:
+                        arg = event.arg[0]
+                        msg = event.arg[1:]
 
-			if arg == "start":
-				self.cah_start(server, event, bot)
-				
-			elif arg == "new":
-				self.cah_new(server, event, bot)
+                        if arg == "start":
+                                self.cah_start(server, event, bot)
+                                
+                        elif arg == "new":
+                                self.cah_new(server, event, bot)
 
-			elif arg == "join":
-				self.cah_join(server, event, bot)
+                        elif arg == "join":
+                                self.cah_join(server, event, bot)
 
-			elif arg == "end":
-				self.cah_end(server, event, bot)
+                        elif arg == "end":
+                                self.cah_end(server, event, bot)
 
-			elif arg == "add":
-				if len(msg) >= 2:
-					color = msg[0]
-					body = ' '.join(msg[1:])
-					self.cah_add(server, event, bot, color, body)
-				else:
-					usage = "Add card usage"
-					server.privmsg(event.target, usage)
+                        elif arg == "add":
+                                if len(msg) >= 2:
+                                        color = msg[0]
+                                        body = ' '.join(msg[1:])
+                                        self.cah_add(server, event, bot, color, body)
+                                else:
+                                        usage = "Add card usage"
+                                        server.privmsg(event.target, usage)
 
-			elif arg == "select":
-				if len(msg) >= 1:
-					cards = msg[0:]
-					self.cah_select(server, event, bot, cards)
-				else:
-					usage = "Select card usage"
-					server.privmsg(event.target, usage)
+                        elif arg == "select":
+                                if len(msg) >= 1:
+                                        cards = msg[0:]
+                                        self.cah_select(server, event, bot, cards)
+                                else:
+                                        usage = "Select card usage"
+                                        server.privmsg(event.target, usage)
 
-			elif arg == "vote":
-				if len(msg) == 1:
-					#going to need some args here
-					self.cah_vote(server, event, bot, msg[0])
-				else:
-					usage = "Vote usage"
-					server.privmsg(event.target, usage)
+                        elif arg == "vote":
+                                if len(msg) == 1:
+                                        #going to need some args here
+                                        self.cah_vote(server, event, bot, msg[0])
+                                else:
+                                        usage = "Vote usage"
+                                        server.privmsg(event.target, usage)
 
-		else:
-			server.privmsg(event.target, usage)
+                else:
+                        server.privmsg(event.target, usage)
 
 ################################################################################
 # Function prototypes to fill in
 ################################################################################
 
-	def cah_new(self, server, event, bot):
-		'''Initialize a new game.'''
-		#check if game is already running
-		if self.game.running:
-			msg = "A game is already running!  Use '@cah end' to end it."
-		else:
-			msg = "A new game of Cards Against Humanity has started!  Use '%scah join' to join the game." % server.cmdchar
-			self.game = CAHGame()
-			self.game.running = True
-		server.privmsg(event.target, msg)
-		
-	def cah_start(self, server, event, bot):
-		'''Start the game once all players have joined..'''
-		server.privmsg(event.target, "PLACEHOLDER: starting new game")
-		
-	def cah_join(self, server, event, bot):
-		'''Join the current game.'''
-		# make this error check and suck less
-		server.privmsg(event.target, "PLACEHOLDER: joining the game")
-		self.game.add_player(event.source.split('!')[0])
-		server.privmsg(event.target, "%s joined game." % self.game.players[len(self.game.players)-1].name)
+        def cah_new(self, server, event, bot):
+                '''Initialize a new game.'''
+                #check if game is already running
+                if self.game.running:
+                        msg = "A game is already running!  Use '@cah end' to end it."
+                else:
+                        msg = "A new game of Cards Against Humanity has started!  Use '%scah join' to join the game." % server.cmdchar
+                        self.game = CAHGame()
+                        self.game.running = True
+                server.privmsg(event.target, msg)
+                
+        def cah_start(self, server, event, bot):
+                '''Start the game once all players have joined..'''
+                server.privmsg(event.target, "PLACEHOLDER: starting new game")
+                
+        def cah_join(self, server, event, bot):
+                '''Join the current game.'''
+                # make this error check and suck less
+                server.privmsg(event.target, "PLACEHOLDER: joining the game")
+                self.game.add_player(event.source.split('!')[0])
+                server.privmsg(event.target, "%s joined game." % self.game.players[len(self.game.players)-1].name)
 
-	def cah_end(self, server, event, bot):
-		'''Abort the current game.'''
-		#check if game is already running
-		if self.game.running:
-			msg = "The game has ended."
-			self.game.running = False
-		else:
-			msg = "There's no game running!  Use '@cah new' to start a new game."
-			
-		server.privmsg(event.target, msg)
+        def cah_end(self, server, event, bot):
+                '''Abort the current game.'''
+                #check if game is already running
+                if self.game.running:
+                        msg = "The game has ended."
+                        self.game.running = False
+                else:
+                        msg = "There's no game running!  Use '@cah new' to start a new game."
+                        
+                server.privmsg(event.target, msg)
 
-	def cah_add(self, server, event, bot, color, body):
-		'''Add a card to the database.'''
-		server.privmsg(event.target, "PLACEHOLDER: adding %s card: %s" % (color, body))
+        def cah_add(self, server, event, bot, color, body):
+                '''Add a card to the database.'''
+                server.privmsg(event.target, "PLACEHOLDER: adding %s card: %s" % (color, body))
 
-	def cah_select(self, server, event, bot, cards):
-		'''Select card(s) to play from your hand.'''
-		server.privmsg(event.target, "PLACEHOLDER: selecting cards %s" % ', '.join(cards))
+        def cah_select(self, server, event, bot, cards):
+                '''Select card(s) to play from your hand.'''
+                server.privmsg(event.target, "PLACEHOLDER: selecting cards %s" % ', '.join(cards))
 
-	def cah_vote(self, server, event, bot, voted):
-		'''Czar voting for group #.'''
-		server.privmsg(event.target, "PLACEHOLDER: czar is voting for %s" % voted)
+        def cah_vote(self, server, event, bot, voted):
+                '''Czar voting for group #.'''
+                server.privmsg(event.target, "PLACEHOLDER: czar is voting for %s" % voted)
 
         def cah_init(self, server, event, bot):
             server.privmsg(event.target, "Adding cards to DB")
