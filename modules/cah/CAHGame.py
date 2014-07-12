cee291799e9aa19e23593b3618a45f7cee16d0ed
Landon Fowles <landon@lfowles.org>
2014-07-11 23:30:48 -0500

    Use the new Deck class

--- modules/cah/CAHGame.py
+++ modules/cah/CAHGame.py
@@ -1,5 +1,7 @@
 #Cards Against Humanity game engine
 
+from cards import Deck, NoMoreCards
+
 class CAHGame:
         def __init__(self):
                 
@@ -13,33 +15,14 @@ class CAHGame:
                 
                 #dummy with a small deck for testing.
                 #replace with actual card loading from DB later
-                self.deck ={
-                        'questions' : [
-                                "_? There's an app for that.",
-                                "I got 99 problems but _ ain't one.",
-                        ],
-                        'answers' : [
-                                "Flying sex snakes.",
-                                "Michelle Obama's arms.",
-                                "German dungeon porn.",
-                                "White people.",
-                                "Getting so angry that you pop a boner.",
-                                "Freedom of Speech",
-                        ]
-                }
-                
-                
+                self.deck = Deck()
+
         #add a new player to the game
         def add_player(self, name):
                 self.players.append(Player(name))
                 
         #start the game
         def start(self):
-                pass
-                
-
-        def draw(self, color):
-                '''Draws a random card of <color> from the databse and returns a Card object.'''
                 pass
 
 
@@ -50,9 +33,3 @@ class Player:
                 self.score = 0
                 self.hand = {}
                 self.isCzar = False
-                
-#Utiliy class for a Card
-class Card:
-        def __init__(self, color, body):
-                self.color = color
-                self.body = body
